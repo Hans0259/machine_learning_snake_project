c4cf41716c2f2b4e25045cfdf2125e2f74d8b1ca
Weichen Hou <128548454+Hans0259@users.noreply.github.com>
2025-11-29 23:44:18 -0500

    Delete 4al3 directory

--- 4al3/test.py
+++
@@ -1,5 +0,0 @@
-import torch
-print("Torch:", torch.__version__)
-print("CUDA in torch:", torch.version.cuda)
-print("CUDA available:", torch.cuda.is_available())
-print("GPU name:", torch.cuda.get_device_name(0))
